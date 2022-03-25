d3fd15cb6e2d4d7cbbe84dfe7257fb485e4f29c6
Aily-Hhh <aily.hhh13@gmail.com>
2022-03-25 15:13:00 +0300

    3 пункт 1 задания

---
+++ Task_1.3/Program.cs
@@ -0,0 +1,24 @@
+using Task_1._3;
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+List<Person> people = new List<Person>();
+
+app.Map("/", async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.SendFileAsync("html/index.html");
+});
+
+app.Map("/postuser", async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    var form = context.Request.Form;
+    string name = form["name"];
+    string age = form["age"];
+    await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+
+});
+
+app.Run();
